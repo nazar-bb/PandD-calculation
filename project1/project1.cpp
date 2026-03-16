49b3b5e654c644251d475b480b03809e2fe16496
nazar-bb <nazar.babuch23@gmail.com>
2026-03-16 11:55:25 +0200

    mnoj2matrix

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -8,7 +8,6 @@
 
 int main() {
   srand(time(0));
-
   int size;
   int min;
   int max;
@@ -28,11 +27,34 @@ auto MatrixRandom = [](int size, int min, int max) {
         }
         return mt;
     };
+auto Mnoj = [&](int size, int min, int max) {
+        auto mt1 = MatrixRandom(size, min, max);
+        auto mt2 = MatrixRandom(size, min, max);
+        int sizelocal = mt1.size();
 
-auto mt1 = MatrixRandom(size, min, max);
-auto mt2 = MatrixRandom(size, min, max);
-
-
-
-
+        std::vector<std::vector<int>> result(sizelocal, std::vector<int>(sizelocal, 0));
+        for (int i = 0; i < sizelocal; i++) {
+            for (int j = 0; j < sizelocal; j++) {
+                for (int k = 0; k < sizelocal; k++) {
+                    result[i][j] += mt1[i][k] * mt2[k][j];
+                }
+            }
+        }
+        return result;
+};
+#pragma omp parallel num_threads(2)
+{
+    auto Result = Mnoj(size, min, max);
+    #pragma omp critical
+{
+    std::cout << "\nПотік " << omp_get_thread_num() << " результат:\n";
+    for (int i = 0; i < Result.size(); i++) {
+        for (int j = 0; j < Result[i].size(); j++) {
+            // Виводимо число і просто додаємо табуляцію для відступу
+            std::cout << Result[i][j] << "\t";
+        }
+        std::cout << "\n"; // Перехід на новий рядок
+    }
+}
+}
 }

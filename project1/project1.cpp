42e0174c099e67988a68d02771ea2302d5e7820d
nazar-bb <nazar.babuch23@gmail.com>
2026-03-16 12:34:27 +0200

    Final1

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -7,15 +7,16 @@
 
 
 int main() {
+  omp_set_nested(1);
   srand(time(0));
-  int size;
-  int min;
-  int max;
+  int size = 2000;
+  int min = -1000;
+  int max = 1000;
 
-do {
-  std::cout << "Параметри матриці (size, min, max)" << std::endl;
-  std::cin >> size >> min >> max;
-} while(min > max);
+//do {
+//  std::cout << "Параметри матриці (size, min, max)" << std::endl;
+//  std::cin >> size >> min >> max;
+//} while(min > max);
 
 
 auto MatrixRandom = [](int size, int min, int max) {
@@ -27,12 +28,15 @@ auto MatrixRandom = [](int size, int min, int max) {
         }
         return mt;
     };
+
 auto Mnoj = [&](int size, int min, int max) {
         auto mt1 = MatrixRandom(size, min, max);
         auto mt2 = MatrixRandom(size, min, max);
         int sizelocal = mt1.size();
 
+
         std::vector<std::vector<int>> result(sizelocal, std::vector<int>(sizelocal, 0));
+        #pragma omp parallel for num_threads(sizelocal)
         for (int i = 0; i < sizelocal; i++) {
             for (int j = 0; j < sizelocal; j++) {
                 for (int k = 0; k < sizelocal; k++) {
@@ -42,19 +46,11 @@ auto Mnoj = [&](int size, int min, int max) {
         }
         return result;
 };
+double start = omp_get_wtime();
 #pragma omp parallel num_threads(2)
 {
     auto Result = Mnoj(size, min, max);
-    #pragma omp critical
-{
-    std::cout << "\nПотік " << omp_get_thread_num() << " результат:\n";
-    for (int i = 0; i < Result.size(); i++) {
-        for (int j = 0; j < Result[i].size(); j++) {
-            // Виводимо число і просто додаємо табуляцію для відступу
-            std::cout << Result[i][j] << "\t";
-        }
-        std::cout << "\n"; // Перехід на новий рядок
-    }
 }
-}
+double end = omp_get_wtime();
+std::cout << "\nЗагальний час виконання: " << (end - start) << " сек." << std::endl;
 }
